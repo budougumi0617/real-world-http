f44165ad91d5044821f06f96c22e6439fc235c1a
budougumi0617 <budougumi0617@gmail.com>
2018-06-04 10:41:39 +0900

    :+1: P174 6-13 Add protocol upgrade server

---
+++ ch06/protocolupgrade/server/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"time"
+)
+
+func handlerUpgrade(w http.ResponseWriter, r *http.Request) {
+	// The endpoint refuses connect other than upgrade.
+	if r.Header.Get("Connection") != "Upgrade" || r.Header.Get("Upgrade") != "MyProtocol" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	fmt.Println("Upgrade to MyProtocol")
+
+	// Get low layer socket
+	hijacker := w.(http.Hijacker)
+	conn, readWriter, err := hijacker.Hijack()
+	if err != nil {
+		panic(err)
+		return
+	}
+	defer conn.Close()
+
+	// Send response about protocol is changed.
+	response := http.Response{
+		StatusCode: 101,
+		Header:     make(http.Header),
+	}
+	response.Header.Set("Upgrade", "MyProtocol")
+	response.Header.Set("Connection", "Upgrade")
+	response.Write(conn)
+
+	// Start original communication
+	for i := 1; i <= 10; i++ {
+		fmt.Fprintf(readWriter, "%d\n", i)
+		fmt.Println("->", i)
+		readWriter.Flush() // Trigger "chunked" encoding and send a chunk...
+		recv, err := readWriter.ReadBytes('\n')
+		if err == io.EOF {
+			break
+		}
+		fmt.Printf("<- %s", string(recv))
+		time.Sleep(500 * time.Millisecond)
+	}
+}
+func handler(w http.ResponseWriter, r *http.Request) {
+	dump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+		return
+	}
+	fmt.Println(string(dump))
+	fmt.Fprintf(w, "<html><body>hello</body></html>")
+}
+
+func main() {
+	http.HandleFunc("/upgrade", handlerUpgrade)
+	http.HandleFunc("/", handler)
+	log.Println("start http listening :18443")
+	err := http.ListenAndServeTLS(":18443", "../cert/server.crt", "../cert/server.key", nil)
+	log.Println(err)
+}
